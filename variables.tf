6e1cc6c708fa1b0cbab2c2bc3a0c005ea8380cbd
kingsonrisaa <awsingjose@gmail.com>
2023-06-19 18:50:27 -0400

    v13

--- variables.tf
+++ variables.tf
@@ -2,4 +2,12 @@ variable "address" {
   type = string
   description = "Address"
 }
+variable "f5_username" {
+  type = string
+  description = "Address"
+}
+variable "f5_password" {
+  type = string
+  description = "Address"
+}
 
--- variables.tfvars
+++ variables.tfvars
@@ -1,1 +1,3 @@
 address = "https://172.16.253.240"
+f5_username = f5_username
+f5_password = f5_password
