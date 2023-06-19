24dbbc61567bb3f47ce5e9e9328d7d6ec225e3a0
kingsonrisaa <awsingjose@gmail.com>
2023-06-19 16:00:03 -0400

    first commit

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    bigip = {
+      source = "terraform-providers/bigip"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "bigip" {
+  address  = var.address
+}
+
+
