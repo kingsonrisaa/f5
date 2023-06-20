3fb5bbfd22c23a673be2bac60b5faa75866cb29a
kingsonrisaa <awsingjose@gmail.com>
2023-06-20 15:19:40 -0400

    v3449484584509

--- main.tf
+++ main.tf
@@ -8,29 +8,3 @@ resource "bigip_ltm_snatpool" "snatpool_santiago" {
   members = ["191.1.1.1", "194.2.2.2"]
 }
 
-resource "bigip_ltm_policy" "test-policy-test" {
-  name           = "my_policy_test"
-  strategy       = "first-match"
-  requires       = ["http"]
-  published_copy = "Drafts/my_policy"
-  controls       = ["forwarding"]
-  rule {
-    name = "rule6"
-
-    action {
-      tm_name    = "20"
-      forward    = true
-      connection = false
-      pool       = "/Common/mypool"
-    }
-  }
-  depends_on = [bigip_ltm_pool.mypool]
-}
-
-resource "bigip_ltm_pool" "mypool" {
-  name                = "/Common/mypool"
-  monitors            = ["/Common/http"]
-  allow_nat           = "yes"
-  allow_snat          = "yes"
-  load_balancing_mode = "round-robin"
-}
