ea496a6c7e33b2a3f865b7ba497379b7e4a077f7
kingsonrisaa <awsingjose@gmail.com>
2023-06-20 15:07:07 -0400

    commitv45555

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "bigip_ltm_node" "nginx-node1" {
   name    = "/Common/nginx-node1"
   address = "172.16.253.13"
 }
+
+resource "bigip_ltm_snatpool" "snatpool_santiago" {
+  name    = "/Common/snatpool_santiago"
+  members = ["191.1.1.1", "194.2.2.2"]
+}
