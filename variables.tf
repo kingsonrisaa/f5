5b6e7983232dd9ff27d8ae492102efa03d9b4fa9
kingsonrisaa <awsingjose@gmail.com>
2023-06-19 23:27:34 -0400

    v455558

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "f5_password" {
+  description = "Password for F5"
+  type        = string
+}
+
+variable "f5_username" {
+  description = "Password for F5"
+  type        = string
+}
+
+variable "address" {
+  description = "Password for F5"
+  type        = string
+}
