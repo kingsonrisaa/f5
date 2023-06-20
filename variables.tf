e6521664da8a5891a7c51f675f40f5f7bd4a6368
kingsonrisaa <awsingjose@gmail.com>
2023-06-19 20:22:08 -0400

    commitear233

--- variables.tf
+++ variables.tf
@@ -1,1 +1,12 @@
+variable "username" {
+  description = "username for F5"
+}
+
+variable "password" {
+  description = "Password for F5"
+}
+
+variable "address" {
+  description = "address for F5"
+}
 
