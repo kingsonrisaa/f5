32a6c5f65d820cb5a3b3bc4979084e8fd9e9ed3a
kingsonrisaa <awsingjose@gmail.com>
2023-06-22 13:34:39 -0400

    v3232323232

--- main.tf
+++ main.tf
@@ -4,6 +4,14 @@ resource "bigip_ltm_node" "nginx-node1" {
   monitor = "/Common/http-nginx"
 }
 
+resource "bigip_ltm_monitor" "TC1" {
+  name     = "/Common/test_monitor_tc1"
+  parent   = "/Common/http"
+  send     = "GET /some/path\r\n"
+  timeout  = "999"
+  interval = "990"
+}
+
 resource "bigip_ltm_snatpool" "snatpool_santiago" {
   name    = "/Common/snatpool_santiago"
   members = ["191.1.1.1", "194.2.2.2"]
