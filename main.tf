01a7cad8d12c2a6b474ab3b5f5b4a5e2b4acde94
kingsonrisaa <awsingjose@gmail.com>
2023-06-22 13:06:52 -0400

    lav232323

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "bigip_ltm_node" "nginx-node1" {
   name    = "/Common/nginx-node1"
   address = "172.16.253.13"
+  monitor = "/Common/http-nginx"
 }
 
 resource "bigip_ltm_snatpool" "snatpool_santiago" {
