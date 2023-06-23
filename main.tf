ff239bb2bf3ff862b25add34f0ceb22f0c01c97f
kingsonrisaa <awsingjose@gmail.com>
2023-06-23 11:33:51 -0400

    lab39293203j

--- main.tf
+++ main.tf
@@ -10,14 +10,21 @@ resource "bigip_ltm_monitor" "hotel_test" {
   receive =  "HTTP/1.1 200 OK"
 }
 
+
 resource "bigip_ltm_pool" "pool_test" {
   name                   = "/Common/pool_test"
   load_balancing_mode    = "least-connections-member"
-  monitors               = [ bigip_ltm_monitor.hotel_test.name ]  
+  monitors               = [ bigip_ltm_monitor.hotel_test.name ]
   members = [
     {
       name         = "member1"
       address      = "192.168.1.10"
+      port         = 80
+      connection_limit = 100
+    },
+    {
+      name         = "member2"
+      address      = "192.168.1.11"
       port         = 80
       connection_limit = 100
     }
