ead45a593894504d3a2dc8c1d004b67c031476ea
kingsonrisaa <awsingjose@gmail.com>
2023-06-23 11:21:53 -0400

    lab39293203

--- main.tf
+++ main.tf
@@ -14,5 +14,11 @@ resource "bigip_ltm_pool" "pool_test" {
   name                   = "/Common/pool_test"
   load_balancing_mode    = "least-connections-member"
   monitors               = [ bigip_ltm_monitor.hotel_test.name ]
+  members = [
+    {
+      name         = "member1"
+      address      = "192.168.1.10"
+      port         = 80
+      connection_limit = 100
+    }
 }
-
