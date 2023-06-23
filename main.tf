153f8cb2fb63f000eaa477fe7b0a137838d6472d
kingsonrisaa <awsingjose@gmail.com>
2023-06-23 10:48:27 -0400

    lab32323

--- main.tf
+++ main.tf
@@ -4,26 +4,15 @@ resource "bigip_ltm_node" "nginx-node1" {
   monitor = "/Common/http-nginx"
 }
 
-resource "bigip_ltm_monitor" "TC1" {
-  name     = "/Common/test_monitor_tc1"
-  parent   = "/Common/http"
-  send     = "GET /some/path\r\n"
-  timeout  = "999"
-  interval = "990"
-}
-
 resource "bigip_ltm_monitor" "hotel_test" {
   name     = "/Common/hotel_test"
   parent   = "/Common/https"
   receive =  "HTTP/1.1 200 OK"
 }
-resource "bigip_ltm_snatpool" "snatpool_santiago" {
-  name    = "/Common/snatpool_santiago"
-  members = ["191.1.1.1", "194.2.2.2"]
-}
 
 resource "bigip_ltm_pool" "pool_test" {
   name                   = "/Common/pool_test"
   load_balancing_mode    = "least-connections-member"
-  monitors               = ["hotel_test"]
+  monitors               = [ bigip_ltm_monitor.hotel_test.name ]
 }
+
