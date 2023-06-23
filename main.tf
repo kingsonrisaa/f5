ea747f656ce9189ff34c272da05b76e55047b551
kingsonrisaa <awsingjose@gmail.com>
2023-06-23 16:10:32 -0400

    lab30293923

--- main.tf
+++ main.tf
@@ -1,20 +1,22 @@
-resource "bigip_ltm_node" "nginx-node1" {
-  name    = "/Common/nginx-node1"
-  address = "172.16.253.13"
-  monitor = bigip_ltm_monitor.hotel_test.id
-}
-
 resource "bigip_ltm_monitor" "hotel_test" {
   name     = "/Common/hotel_test"
   parent   = "/Common/http"
   receive =  "HTTP/1.1 200 OK"
 }
-resource "bigip_ltm_pool_attachment" "attach-nginx-pool" {
-  pool = bigip_ltm_pool.pool_test.name
-  node = "${bigip_ltm_node.nginx-node1.name}:80"
-}
+
 resource "bigip_ltm_pool" "pool_test" {
   name                   = "/Common/pool_test"
   load_balancing_mode    = "least-connections-member"
-  monitors               = [ bigip_ltm_monitor.hotel_test.name ]
+  monitors               = [bigip_ltm_monitor.hotel_test.name]
+}
+
+resource "bigip_ltm_node" "nginx-node1" {
+  name    = "/Common/nginx-node1"
+  address = "172.16.253.13"
+  monitor = bigip_ltm_monitor.hotel_test.name
+}
+
+resource "bigip_ltm_pool_attachment" "attach-nginx-pool" {
+  pool = bigip_ltm_pool.pool_test.name
+  node = "${bigip_ltm_node.nginx-node1.name}:80"
 }
