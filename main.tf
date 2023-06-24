53ede1a2d436980b06d6723e9ed4caa434a908af
kingsonrisaa <awsingjose@gmail.com>
2023-06-24 01:24:29 -0400

    lab3yu8u8ydwdkkiksjddsssjxxdddddsssssss

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "bigip_ltm_pool" "pool_test" {
 resource "bigip_ltm_node" "nginx-node1" {
   name    = "/Common/nginx-node1"
   address = "172.16.253.13"
-  monitor = "/Common/http"
+  monitor = "/Common/icmp"
 }
 
 resource "bigip_ltm_pool_attachment" "attach-nginx-pool" {
