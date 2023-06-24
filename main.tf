c4fdcd4a9bf82a35d40f936232083ef3581e6c9c
kingsonrisaa <awsingjose@gmail.com>
2023-06-24 00:55:05 -0400

    lab3yu8u8ydwdkkiksjddj

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
 resource "bigip_ltm_node" "nginx-node1" {
   name    = "/Common/nginx-node1"
   address = "172.16.253.13"
-  monitor          = "/Common/icmp"
+  monitor = "bigip_ltm_monitor.hotel_test.name"
 }
 
+resource "bigip_ltm_monitor" "hotel_test" {
+  name     = "/Common/hotel_test"
+  parent   = "/Common/http"
+  receive =  "HTTP/1.1 200 OK"
+}
